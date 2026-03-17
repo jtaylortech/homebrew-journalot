e7b972ec06bf4c9053fe832769a4c78e6c3e87fd
jtaylortech <jarrede20@gmail.com>
2026-03-16 23:57:33 -0400

    journalot 5.2.1

--- Formula/journalot.rb
+++ Formula/journalot.rb
@@ -1,8 +1,8 @@
 class Journalot < Formula
   desc "Git-backed journaling CLI for developers"
   homepage "https://github.com/jtaylortech/journalot"
-  url "https://github.com/jtaylortech/journalot/archive/refs/tags/v5.2.0.tar.gz"
-  sha256 "38aa70dae0f2fd547dd584f178d4428a798cd723ad820c30419b0e5d514cc549"
+  url "https://github.com/jtaylortech/journalot/archive/refs/tags/v5.2.1.tar.gz"
+  sha256 "1c4f93eda09312891e3c3d8d1f7ed0844df4b7c3399407b99ec47f13dc20a188"
   license "MIT"
 
   def install
